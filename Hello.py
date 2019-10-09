bc98a3f237607aa0699d05d09978a6144d3f6454
PAULINA LIWOSZ <paliwosz@IN.THE.ANTHILL>
2019-10-09 12:23:25 +0200

    Added Hello.py

---
+++ Hello.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+print('Hello World')
+name = 'Paulina'
+print(f"My name is {name}")
+
+a=12
+b=5
+print(f" To : {a} jest wieksze niż to: {b}")
